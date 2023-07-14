33ec438535b9512e2ec1979ccd2127c64644244c
lennyweda <lennyweda@gmail.com>
2023-07-14 16:22:14 +0300

    (master): additional topics

--- Pattern matching/pattern_matching.rs
+++ Pattern matching/pattern_matching.rs
@@ -55,6 +55,31 @@ fn main(){
         println!("The option has no value");
     }
 
+    let number = 4;
+
+    println!("Tell me about {}", number);
+    match number {
+        // Match a single value
+        1 => println!("One!"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11| 13 => println!("This is a prime"),
+        // Match an inclusive range
+        13..=19 => println!("A teen"),
+        
+        _ => println!("Ain't special"),
+        // TODO ^ Try commenting out this catch-all arm
+    }
+
+    // note that match is an expression too:
+    let boolean = true;
+    // Match is an expression too
+    let binary = match boolean {
+        // The arms of a match must cover all the possible values
+        false => 0,
+        true => 1,
+        // TODO ^ Try commenting out one of these arms
+    };
+    println!("{} -> {}", boolean, binary);
 
 
 }
--- control-flow/loops.rs
+++ control-flow/loops.rs
@@ -1,13 +1,17 @@
+#![allow(unreachable_code)]
 fn main(){
     /* infinite loop:  */
     let mut y = 0;
-    loop{
+    let result = loop{
         y += 1;
         println!("looping!!");
-        if y > 1000{
-            break;
+        if y > 10{
+            break y;
         }
-    }
+    };
+
+    // returning a value from a loop: 
+    println!("The result returned from the loop is -> {}", result);
 
     /* while loop:  */
     y = 0;
@@ -16,11 +20,32 @@ fn main(){
         println!("{y}");
     }
 
-    /* for loop */
+    /* for loop 
+        for exclusive, use 1..6
+    */
 
-    for i in 1..6{
+    for i in 1..=5{
         println!(" Number is -> {i}");
     }
+
+    //breaking out of an outer loop:
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
 
     
 
