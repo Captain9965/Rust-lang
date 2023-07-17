b395618279eaa7a53e83cc8d6c70db4e1b3ca68b
lennyweda <lennyweda@gmail.com>
2023-07-17 16:26:11 +0300

    attributes in Rust

---
+++ Attributes/attributes.rs
@@ -0,0 +1,10 @@
+/*
+    Attributes are used for:
+        1. conditional compilation
+        2. disable warnings
+        3. link to a foreign library
+        4. set crate name, library, and type
+        5. 
+
+
+ */
